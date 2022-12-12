da64e81e72a6c12ad0e56b5cad52d469d2ec4e0e
HieuDang <hieu37.nghiadung@gmail.com>
2022-12-12 10:45:24 +0700

    test

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
-const port = 3000
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const port = 3000;
 
-app.use(morgan('combined'))
+app.use(morgan("combined"));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 //localhost - 127.0.0.1
 
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
