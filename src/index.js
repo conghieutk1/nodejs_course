12e26734de963e8a11f2dc7daec9bc2435218a64
HieuDang <hieu37.nghiadung@gmail.com>
2022-12-16 18:15:32 +0700

    16_12

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+require('dotenv').config();
 //const handlebars = require('express-handlebars');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8888;
+const hostname = process.env.HOST_NAME;
 
 // HTTP loggers
 app.use(morgan('combined'));
 
 //Template engine
 
-//app.engine('handlebars', handlebars.engine);
-app.set('view', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+console.log(path.join(__dirname + '/views'));
 
 app.get('/', (req, res) => {
-  // res.send("Hello World!");
+  //res.send('Hello World!');
   res.render('sample.ejs');
 });
 //localhost - 127.0.0.1
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
 });
